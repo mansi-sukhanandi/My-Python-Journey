b7c3449355594b12508dbab4079a93f60d47ce2b
Mansi <mk.sukhanandi@gmail.com>
2026-04-27 13:04:47 +0530

    Fix comments in ML (Linear Regression) script
    Corrected comments in the Linear Regression script.

--- ML (Linear Regression).py
+++ ML (Linear Regression).py
@@ -1,7 +1,7 @@
 # Step 1: Library bulai
 from sklearn.linear_model import LinearRegression
 
-# Step 2: Data taiyar kiya (X hamesha 2D hota hai)
+# Step 2: Data taiyar kiya 
 X = [[1], [2], [3]]  # Experience in years
 y = [30000, 45000, 60000] # Salary in Rupees
 
