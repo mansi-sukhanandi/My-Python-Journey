3b6eb1e73d13bca94795452c5e52914888475e47
Mansi <mk.sukhanandi@gmail.com>
2026-05-05 13:38:52 +0530

    Add files via upload

---
+++ ML (Linear Regression).py
@@ -0,0 +1,18 @@
+# Step 1: Library bulai
+from sklearn.linear_model import LinearRegression
+
+# Step 2: Data taiyar kiya (X hamesha 2D hota hai)
+X = [[1], [2], [3]]  # Experience in years
+y = [30000, 45000, 60000] # Salary in Rupees
+
+# Step 3: Model ka Object banaya (OOPS!)
+model = LinearRegression()
+
+# Step 4: Machine ko sikhaya (Training)
+model.fit(X, y)
+
+# Step 5: Prediction mangi (Test)
+exp_saal = [[5]] # Agar 5 saal ka experience ho toh?
+salary_prediction = model.predict(exp_saal)
+
+print(f"5 saal experience ki estimated salary hai: {salary_prediction[0]}")
